fc882b4fd87addc73f83a8f44218b29c931e967f
unkowed-jck <ahhasvbsd@gmail.com>
2026-06-19 15:05:56 +0700

    coba lagi2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,10 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        //
+{
+    if ($this->app->environment('production')) {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
+}
+    
 }
